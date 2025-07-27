9041703e7806dc449fedefbf58af6d06ac3b7cd3
Alquieri <Guilherme.alquieri@gmail.com>
2025-07-27 14:29:47 -0300

    Creating project

---
+++ backend/Program.cs
@@ -0,0 +1,25 @@
+using System.Net.Security;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello-word", () =>
+{
+    return "Hello word!";
+})
+
+.WithOpenApi();
+app.Run();
+
