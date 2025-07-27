746786dd9de2f4b1cf9cca90cdb3a3fd70a1e615
Alquieri <Guilherme.alquieri@gmail.com>
2025-07-27 15:05:56 -0300

    Add model Part

---
+++ backend/Models/Part.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace backend.Models
+{
+    public class Part
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+        public Part(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
