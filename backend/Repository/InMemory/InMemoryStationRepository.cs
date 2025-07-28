3632d93ff127dc7135f9081d49824322e08010f3
Alquieri <Guilherme.alquieri@gmail.com>
2025-07-28 14:47:50 -0300

    Add MemoryStation

---
+++ backend/Repository/InMemory/InMemoryStationRepository.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+
+namespace backend.Repository.InMemory
+{
+    public class InMemoryStationRepository
+    {
+        private readonly List<Station> _station = new(); //Finge que é um banco de dados
+
+        public IEnumerable<Station> GetAll()
+        {
+            return _station;
+        }
+
+        public Station GetById(Guid id)
+        {
+            var result = _station.FirstOrDefault(p => p.Id == id);
+
+            if (result == null)
+                throw new Exception($"Station with id '{id}' not found.");
+
+            return result;
+        }
+
+        public void Add(Station station)
+        {
+            _station.Add(station);
+
+        }
+
+        public void Updade(Station station)
+        {
+
+            var index = _station.FindIndex(s => s.Id == station.Id);
+            if (index != -1)
+            _station[index] = station;
+
+        }
+
+        public void Delete(Guid id)
+        {
+            var station = GetById(id);
+            if (station != null)
+            _station.Remove(station);
+            
+        }
+
+
+    }
+}
