a54b137e0b0c3a0e0dd812db9001ea09f04f0ffa
Alquieri <Guilherme.alquieri@gmail.com>
2025-07-30 17:08:33 -0300

    Perform code cleanup

--- backend/Repository/IMovementRepository.cs
+++ backend/Repository/IMovementRepository.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
 using backend.Models;
 
 namespace backend.Repository
--- backend/Services/PartService.cs
+++ backend/Services/PartService.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+
 using backend.Models;
 using backend.Repository;
 
